b0c8481feae9f9e3cdd3248977e885716dee3abe
Evgeniy Zotov <e.zotov@carx-tech.com>
2021-11-23 18:43:19 +0700

    Revert "Перенес фронт в отдельный репозиторий"
    This reverts commit 3f5b82a6cb13694230e92e79164ac063c4373913.

--- 20211115-otus-spring-zotov-spring-mvc-hw10/frontend-js/book-app/src/component/author/AuthorEdit.js
+++
@@ -1,9 +0,0 @@
-import {SimpleForm, TextInput} from "react-admin";
-import React from "react";
-
-export const AuthorEdit = (props)=>(
-    <SimpleForm >
-        <TextInput disabled source="id" label="Ид"/>
-        <TextInput source="fio" label="ФИО"/>
-    </SimpleForm>
-)
