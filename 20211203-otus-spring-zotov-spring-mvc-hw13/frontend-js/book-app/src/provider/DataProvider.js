eb52910d795fffd24112e272a5d917539e1d6933
Evgeniy Zotov <e.zotov@carx-tech.com>
2021-12-07 19:18:22 +0700

    Проставил во все запросы токен

--- 20211203-otus-spring-zotov-spring-mvc-hw13/frontend-js/book-app/src/provider/DataProvider.js
+++ 20211203-otus-spring-zotov-spring-mvc-hw13/frontend-js/book-app/src/provider/DataProvider.js
@@ -7,7 +7,7 @@ export const dataProvider = {
     getList: (resource) => {
         const url = `${apiUrl}/${resource}`
 
-        return httpClient(url).then(({json}) => ({
+        return httpClient(url, getOptions()).then(({json}) => ({
             data: json,
             total: json.length,
         }));
@@ -16,25 +16,27 @@ export const dataProvider = {
     getMany: (resource) => {
         const url = `${apiUrl}/${resource}`
 
-        return httpClient(url).then(({json}) => ({
+        return httpClient(url, getOptions()).then(({json}) => ({
             data: json,
             total: json.length,
         }));
     },
 
     getOne: (resource, params) =>
-        httpClient(`${apiUrl}/${resource}/${params.id}`).then(({json}) => ({
+        httpClient(`${apiUrl}/${resource}/${params.id}`, getOptions()).then(({json}) => ({
             data: json,
         })),
 
     update: (resource, params) =>
         httpClient(`${apiUrl}/${resource}`, {
+            headers: getHeaders(),
             method: 'PUT',
             body: JSON.stringify(params.data),
         }).then(({json}) => ({data: json})),
 
     create: (resource, params) =>
         httpClient(`${apiUrl}/${resource}`, {
+            headers: getHeaders(),
             method: 'POST',
             body: JSON.stringify(params.data),
         }).then(({json}) => ({
@@ -43,14 +45,34 @@ export const dataProvider = {
 
     delete: (resource, params) =>
         httpClient(`${apiUrl}/${resource}`, {
+            headers: getHeaders(),
             method: 'DELETE',
             body: JSON.stringify([params.id]),
-        }).then(({json}) => ({data: []})),
+        }).then(() => ({data: []})),
 
     deleteMany: (resource, params) =>
         httpClient(`${apiUrl}/${resource}`, {
+            headers: getHeaders(),
             method: 'DELETE',
             body: JSON.stringify(params.ids)
-        }).then(({json}) => ({data: []}))
+        }).then(() => ({data: []}))
 };
 
+/**
+ * Обернуть заголовок в объект опций
+ * @returns {{headers: Headers}} объект опций
+ */
+function getOptions() {
+    return {headers: getHeaders()};
+}
+
+/**
+ * Получить заголовок с токеном
+ * @returns {Headers} заголовки
+ */
+function getHeaders() {
+    const token = localStorage.getItem('auth');
+    const headers = new Headers({Accept: 'application/json'})
+    headers.set('Authorization', `Bearer ${token}`);
+    return headers;
+}
